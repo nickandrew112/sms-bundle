b4e4f6d21827ba9431ccf5af86d3aa183e05356f
joe <nickandre@yandex.ru>
2016-01-13 22:02:44 +0600

    add abstract classes

---
+++ src/SmsBundle/Models/Sms/AbstractClasses/AbstractSmsManager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joe
+ * Date: 13.01.16
+ * Time: 21:58
+ */
+namespace SmsBundle\Model\Sms\AbstractClasses ;
+
+use SmsBundle\Model\Sms\SmsManagerInterface;
+
+abstract class AbstractSmsManager implements SmsManagerInterface {
+
+}
---
+++ src/SmsBundle/Models/Sms/AbstractClasses/AbstractSmsMessage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joe
+ * Date: 13.01.16
+ * Time: 21:57
+ */
+namespace SmsBundle\Model\Sms\AbstractClasses ;
+use SmsBundle\Model\Sms\SmsManagerInterface;
+
+abstract class AbstractSmsMessage implements SmsManagerInterface {
+
+}
