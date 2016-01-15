5723f51e4d90c8b6599ad3989ebcde26ba19afae
joe <nickandre@yandex.ru>
2016-01-15 16:04:33 +0300

    add managers

---
+++ src/SmsBundle/Models/Sms/SmsRu/SmsRuManager.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joe
+ * Date: 13.01.16
+ * Time: 21:53
+ */
+namespace SmsBundle\Model\Sms\SmsRu ;
+
+
+use SmsBundle\Model\Sms\AbstractClasses\AbstractSmsManager;
+use SmsBundle\Model\Sms\SmsManagerInterface;
+
+class SmsRuManager extends AbstractSmsManager {
+
+	/**
+	 * Отправляем сообщение
+	 * @return SmsManagerInterface
+	 */
+	public function send()
+	{
+		// TODO: Implement send() method.
+	}
+}
---
+++ src/SmsBundle/Models/Sms/SmsRu/SmsRuSubscriber.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joe
+ * Date: 13.01.16
+ * Time: 22:06
+ */
+
+namespace SmsBundle\Model\Sms\SmsRu;
+
+
+use SmsBundle\Model\Sms\AbstractClasses\AbstractSmsSubscriber;
+use SmsBundle\Model\Sms\SmsSubscriberInterface;
+
+class SmsRuSubscriber extends AbstractSmsSubscriber {
+
+}
