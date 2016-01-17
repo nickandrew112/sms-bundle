a9084763e54f750cd735df61cf6cb83d4ac4669c
joe <nickandre@yandex.ru>
2016-01-17 22:43:17 +0600

    update composer.json

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Sensio\Bundle\BuzzBundle\SensioBuzzBundle(),
             new AppBundle\AppBundle(),
             new SmsBundle\SmsBundle(),
         ];
